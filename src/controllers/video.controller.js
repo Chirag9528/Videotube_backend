74417ea12333b905c96aea36cc04dd3aaf3b0516
Chirag9528 <chiragvarshney9528@gmail.com>
2024-12-19 12:55:49 +0530

    Add getAllVideos controller in video.controller.js

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -9,8 +9,63 @@ import {uploadOnCloudinary} from "../utils/cloudinary.js"
 
 const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
-    //TODO: get all videos based on query, sort, pagination
-    
+    //  get all videos based on query, sort, pagination
+    const videos = await Video.aggregate([
+        {
+            $match : {
+                $and : [
+                    {
+                        $or: [{title : {$regex : query , $options:"i"}},
+                        {description : {$regex :  query , $options:"i"}}
+                        ]
+                    },
+                    {
+                        owner : new mongoose.Types.ObjectId(userId)
+                    }
+                ]
+            }
+        },
+        {
+            $lookup : {
+                from : "users",
+                localField : "owner",
+                foreignField: "_id",
+                as: "createdBy"
+            }
+        },
+        {
+            $unwind : "$createdBy"
+        },
+        {
+            $project : {
+                thumbnail : 1,
+                videoFile : 1,
+                title: 1,
+                description : 1,
+                createdBy: {
+                    username : 1,
+                    fullName : 1
+                }
+            }
+        },
+        {
+            $sort : {
+                [sortBy]:sortType === 'asc' ? 1 : -1
+            }
+        },
+        {
+            $skip : (page-1)*limit
+        },
+        {
+            $limit : parseInt(limit)
+        }
+    ])
+
+    return res.status(200)
+    .json(new ApiResponse(200 , 
+        videos,
+        "Videos Fetched Successfully"
+    ))
 })
 
 const publishAVideo = asyncHandler(async (req, res) => {
